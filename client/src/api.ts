3e259b0be1e311e0b330921bae6fd98a826ed694
chenyuhao <cccchenyuhao@gmail.com>
2022-01-04 16:37:40 +0800

    发布到heroku

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,29 +1,28 @@
-import { Stroke } from "./types";
+import { Stroke } from './types';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const createProject = (
   name: string,
   strokes: Stroke[],
   image: string
 ) => {
-  return fetch("/api/projects", {
-    method: "POST",
+  return fetch(`${API_URL}/api/projects`, {
+    method: 'POST',
     headers: {
-      Accepet: "application/json",
-      "Content-Type": "application/json",
+      Accepet: 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       name,
       strokes,
       image,
     }),
-  }).then(res => res.json());
+  }).then((res) => res.json());
 };
 
 export const getProject = (projectId: string) =>
-  fetch(`/api/projects/${projectId}`).then((res) =>
-    res.json()
-  );
+  fetch(`${API_URL}/api/projects/${projectId}`).then((res) => res.json());
 
-  export const getProjects = () =>
-  fetch(`/api/projects/`).then((res) =>
-    res.json()
-  );
+export const getProjects = () =>
+  fetch(`${API_URL}/api/projects/`).then((res) => res.json());
